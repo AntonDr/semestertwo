ffa61011fa25cf19838447367bd597f35a1572e9
AntonDr <31773079+AntonDr@users.noreply.github.com>
2018-04-16 03:11:53 +0300

    done

---
+++ ClassString/StringLibrary.h
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <iostream>
+#include <stdexcept>
+
+class String
+{
+
+public:
+
+	String(const char *);
+
+	String(const String &);
+
+	~String();
+
+	int Length() const;
+
+	char * GetString() const;
+
+	bool IsEmpty() const;
+
+	String operator+(const String &);
+
+	String operator+=(const String &);
+
+	const String &operator=(const String &);
+
+	bool operator==(const String &) const;
+
+	bool operator>(const String &) const;
+
+	bool operator<(const String &) const; 
+
+	bool operator<=(const String &) const;
+
+	bool operator>=(const String &) const;
+	
+	char &operator[](int) const;
+
+private:
+	char * str;
+
+	bool flag;
+
+	void SetString(const char *);
+
+	static char * AllocateMemory(const char *);
+};
+
+#pragma region Constructor & Destructor
+
+String::String(const char * str)
+{
+	SetString(str);
+}
+
+String::String(const String & str)
+{
+	this->SetString(str.GetString());
+}
+
+String::~String()
+{
+	delete[] str;
+}
+
+#pragma endregion
+
+#pragma region PrivateMethods
+
+void String::SetString(const char * str)
+{
+	if (str == nullptr)
+	{
+		throw std::invalid_argument("");
+	}
+
+	if (flag==true)
+	{
+		delete[]this->str;
+	}
+	this->str = AllocateMemory(str);
+
+	flag = true;
+
+	strcpy(this->str, str);
+}
+
+char * String::AllocateMemory(const char * str)
+{
+	if (str == nullptr)
+	{
+		throw std::invalid_argument("String can't be null");
+	}
+
+	return new char[strlen(str) + 1];
+}
+
+#pragma endregion
+
+#pragma region PublicMethods
+
+inline int String::Length() const
+{
+	return strlen(this->str);
+}
+
+char * String::GetString() const
+{
+	char * temp = new char[Length() + 1];
+	strcpy(temp, str);
+	return temp;
+}
+
+inline bool String::IsEmpty() const
+{
+	return this->Length() == 0;
+}
+
+String String::operator+(const String & str)
+{
+	char * temp = new char[this->Length() + str.Length() + 1];
+
+	strcpy(temp, "");
+	strcpy(temp, this->GetString());
+	strcat(temp, str.GetString());
+
+	return String(temp);
+}
+
+String String::operator+=(const String & str)
+{
+	char * temp = new char[this->Length() + str.Length() + 1];
+
+	strcpy(temp, "");
+	strcpy(temp, this->GetString());
+	strcat(temp, str.GetString());
+	this->SetString(temp);
+	return *this;
+}
+
+inline bool String::operator==(const String & str) const
+{
+	return strcmp(this->GetString(), str.GetString()) == 0;
+}
+
+inline const String & String::operator=(const String & str)
+{
+	if (this->str != str.str)
+	{
+		strcpy(this->str, str.GetString());
+		return *this;
+	}
+	else return *this;
+}
+
+inline bool String::operator>(const String & str) const
+{
+	return strcmp(this->GetString(), str.GetString()) < 0;
+}
+
+inline bool String::operator<(const String & str) const
+{
+	return strcmp(this->GetString(), str.GetString()) > 0;
+}
+
+inline bool String::operator<=(const String & str) const
+{
+	return strcmp(this->GetString(), str.GetString()) <= 0;
+}
+
+inline bool String::operator>=(const String & str) const
+{
+	return strcmp(this->GetString(), str.GetString()) >= 0;
+}
+
+inline char & String::operator[](int i) const
+{
+	return str[i];
+}
+
+#pragma endregion
+
+
+
+
+
+
+
+
